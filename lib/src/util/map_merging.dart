db97440043fc64ef47357a607d96e9f8da3f2010
Alex Isaienko <thesonerik@gmail.com>
2022-12-23 02:32:05 +0200

    map_merging.dart: adjusted merging conditions

--- lib/src/util/map_merging.dart
+++ lib/src/util/map_merging.dart
@@ -1,12 +1,16 @@
 // Replaces empty or missing values in map1 with values from map2.
 void mergeMapValues(Map map1, Map map2) {
   for (final key in map2.keys) {
-    if (map1[key] == null ||
-        map1[key] == '' ||
-        map1[key] == const [] ||
-        map1[key] == const <dynamic>{} ||
-        map1[key] == const <dynamic, dynamic>{}) {
+    if (!_isEmpty(map2[key]) && _isEmpty(map1[key])) {
       map1[key] = map2[key];
     }
   }
+}
+
+bool _isEmpty(dynamic value) {
+  return value == null ||
+      value == '' ||
+      value == const [] ||
+      value == const <dynamic>{} ||
+      value == const <dynamic, dynamic>{};
 }
