d317ce26622052e2497fda1b7bded6238fa34500
Alex Isaienko <thesonerik@gmail.com>
2022-12-24 02:40:41 +0200

    fast_contacts.dart: allow specifying which fields to fetch

--- lib/src/fast_contacts.dart
+++ lib/src/fast_contacts.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:fast_contacts/fast_contacts.dart';
 import 'package:flutter/services.dart';
 
 import 'model/contact.dart';
@@ -9,13 +10,38 @@ enum ContactImageSize {
   fullSize,
 }
 
+enum ContactField {
+  // Name-related
+  displayName,
+  namePrefix,
+  givenName,
+  middleName,
+  familyName,
+  nameSuffix,
+
+  // Organization-related
+  company,
+  department,
+  jobDescription,
+
+  // Phone-related
+  phoneNumbers,
+  phoneLabels,
+
+  // Email-related
+  emailAddresses,
+  emailLabels,
+}
+
 class FastContacts {
   static const MethodChannel _channel =
       const MethodChannel('com.github.s0nerik.fast_contacts');
 
-  static Future<List<Contact>> getAllContacts() async {
+  static Future<List<Contact>> getAllContacts({
+    List<ContactField> fields = ContactField.values,
+  }) async {
     final result = await _channel.invokeListMethod<Map>('getAllContacts', {
-      'fields': ['phones', 'emails', 'structuredName', 'organization'],
+      'fields': fields.map((e) => e.name).toList(),
     });
     return result?.map(Contact.fromMap).toList() ?? const [];
   }
